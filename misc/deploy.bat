af9bf92c9c39bbf4a8536f58f2de82ea3a6f7247
vtxf <vtxf@qq.com>
2024-11-04 09:15:10 +0800

    master

--- misc/deploy.bat
+++ misc/deploy.bat
@@ -4,14 +4,14 @@ setlocal enabledelayedexpansion
 rem 设置项目目录
 set "projectDir=.."
 rem 设置更新的远程仓库分支
-set "remoteBranch=origin/main"
+set "remoteBranch=origin/master"
 
 rem 无限循环执行更新操作
 :loop
 rem 进入项目目录
 cd /d %projectDir%
 rem 执行git fetch
-git fetch origin main
+git fetch origin master
 rem 执行git reset
 git reset --hard %remoteBranch%
 echo 已完成一次源码更新，下次更新将在5分钟后...
