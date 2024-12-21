5085549d07ab0b2929dd5c2573e1ec1096cd8219
vtxf <vtxf@qq.com>
2024-12-21 09:23:48 +0800

    fix cmd

--- 双击我自动打开网页版示例集.cmd
+++ 双击我自动打开网页版示例集.cmd
@@ -1,5 +1,5 @@
-IF NOT EXIST .\tools\http-server-vtxf20221104.exe (
-    echo "棣栨浣跨敤闇�瑕佷笅杞絟ttp-server-vtxf20221104.exe锛屼笅杞借緝鎱紝璇疯�愬績绛夊緟.."
+@ IF NOT EXIST .\tools\http-server-vtxf20221104.exe (
+    echo "首次使用需要下载http-server-vtxf20221104.exe，下载较慢，请耐心等待.."
     powershell .\tools\download_http-server.ps1
 )
 start .\tools\http-server-vtxf20221104.exe -p 9123
