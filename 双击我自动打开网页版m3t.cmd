51541f9ca6b33cbd5030473601d43b383ce2efaa
vtxf <vtxf@qq.com>
2024-12-21 09:16:20 +0800

    update cmd

--- 双击我自动打开网页版m3t.cmd
+++ 双击我自动打开网页版m3t.cmd
@@ -1,2 +1,5 @@
+IF NOT EXIST .\tools\http-server-vtxf20221104.exe (
+    powershell .\tools\download_http-server.ps1
+)
 start .\tools\http-server-vtxf20221104.exe -p 9123
 start "" "http://127.0.0.1:9123/app/versions/last/?e"
--- 双击我自动打开网页版示例集.cmd
+++ 双击我自动打开网页版示例集.cmd
@@ -1,2 +1,6 @@
+IF NOT EXIST .\tools\http-server-vtxf20221104.exe (
+    powershell .\tools\download_http-server.ps1
+)
+
 start .\tools\http-server-vtxf20221104.exe -p 9123
 start "" "http://127.0.0.1:9123/web/"
